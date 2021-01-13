dea60f882d2c0aad5e1f602b027145df5c9a0d6a
alerozene <alejandrorv68@gmail.com>
2021-01-13 22:09:08 +0100

    display selected file path

--- audiofilter.cpp
+++ audiofilter.cpp
@@ -20,5 +20,5 @@ void audioFilter::on_loadFile_clicked()
     QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"),
                                                     "C:/",
                                                     tr("Images (*.png *.xpm *.jpg)"));
-    //ui->listWidget->addItems(fileName);
+    ui->plainTextEdit->setPlainText(fileName);
 }
